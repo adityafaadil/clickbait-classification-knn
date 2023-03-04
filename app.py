86dd8158b88d6a94d3b86851bc7e4f175cbdcc1c
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-03-04 21:57:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,55 @@
-import streamlit as st
-import joblib
+# import library yang diperlukan
+import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
 
-# Load model
-model = joblib.load('model/clickbait_knn_model.joblib')
+# load dataset clickbait
+data = pd.read_csv('data_bersih.csv')
 
-# Define TF-IDF vectorizer
-vectorizer = TfidfVectorizer()
+# memisahkan dataset menjadi data training dan data testing
+train_data, test_data, train_label, test_label = train_test_split(data['title'], data['label'], test_size=0.2, random_state=42)
 
-# Define Standard Scaler
+# melakukan vektorisasi teks pada dataset
+vectorizer = TfidfVectorizer(stop_words='indonesian')
+train_features = vectorizer.fit_transform(train_data)
+test_features = vectorizer.transform(test_data)
+
+# melakukan standarisasi fitur
 scaler = StandardScaler()
+train_features = scaler.fit_transform(train_features.toarray())
+test_features = scaler.transform(test_features.toarray())
 
-# Set app title
-st.title('Klasifikasi Berita Clickbait')
+# melakukan cross validation untuk menentukan parameter k
+k_range = range(1, 20)
+k_scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    scores = cross_val_score(knn, train_features, train_label, cv=5)
+    k_scores.append(scores.mean())
 
-# Get input from user
-input_text = st.text_input('Masukkan judul berita')
+# menentukan parameter k yang paling baik
+best_k = k_range[k_scores.index(max(k_scores))]
 
-# Make prediction
-if st.button('Predict'):
-    # Process input text
-    input_text = [input_text]
-    input_vec = vectorizer.transform(input_text)
-    input_scaled = scaler.transform(input_vec)
-    
-    # Make prediction using model
-    prediction = model.predict(input_scaled)
-    
-    # Display prediction
-    if prediction == 1:
-        st.write('Judul berita ini termasuk clickbait')
-    else:
-        st.write('Judul berita ini tidak termasuk clickbait')
+# melatih model KNN dengan parameter k yang paling baik
+knn = KNeighborsClassifier(n_neighbors=best_k)
+knn.fit(train_features, train_label)
+
+# mengevaluasi performa model
+accuracy = knn.score(test_features, test_label)
+
+# menampilkan hasil di aplikasi Streamlit
+st.title('Clickbait Classification with KNN')
+st.write('Accuracy:', accuracy)
+
+text_input = st.text_input('Enter a clickbait title:')
+text_features = vectorizer.transform([text_input])
+text_features = scaler.transform(text_features.toarray())
+prediction = knn.predict(text_features)
+
+if prediction == 0:
+    st.write('Not clickbait')
+else:
+    st.write('Clickbait')
