84d7ecb02c31f5ae479c4584e1c3361990704c5b
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-03-04 22:47:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,32 @@
-# import library yang diperlukan
-import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
 import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
 
-# load dataset clickbait
-data = pd.read_csv('data_bersih.csv')
+# load the KNN model and other artifacts
+model = joblib.load('model/knn_model.pkl')
+vectorizer = joblib.load('model/tfidf_vectorizer.pkl')
+scaler = joblib.load('model/scaler.pkl')
 
-# memisahkan dataset menjadi data training dan data testing
-train_data, test_data, train_label, test_label = train_test_split(data['title'], data['label'], test_size=0.2, random_state=42)
-
-# melakukan vektorisasi teks pada dataset
-vectorizer = TfidfVectorizer()
-train_features = vectorizer.fit_transform(train_data)
-test_features = vectorizer.transform(test_data)
-
-# melakukan standarisasi fitur
-scaler = StandardScaler()
-train_features = scaler.fit_transform(train_features.toarray())
-test_features = scaler.transform(test_features.toarray())
-
-# melakukan cross validation untuk menentukan parameter k
-k_range = range(1, 20)
-k_scores = []
-for k in k_range:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn, train_features, train_label, cv=5)
-    k_scores.append(scores.mean())
-
-# menentukan parameter k yang paling baik
-best_k = k_range[k_scores.index(max(k_scores))]
-
-# melatih model KNN dengan parameter k yang paling baik
-knn = KNeighborsClassifier(n_neighbors=best_k)
-knn.fit(train_features, train_label)
-
-# mengevaluasi performa model
-accuracy = knn.score(test_features, test_label)
-
-# Define the title and subtitle of the app
-st.title('Clickbait Classification with KNN')
-st.write('Enter a clickbait title to classify')
-
-# Create a text input for users to enter a title
-text_input = st.text_input('Title')
-
-# Create a button to classify the title
-classify_button = st.button('Classify')
-
-if classify_button:
-    # Transform the user input using the vectorizer and scaler
+# create a function for prediction
+def predict_clickbait(text_input):
     text_features = vectorizer.transform([text_input])
     text_features = scaler.transform(text_features.toarray())
+    prediction = model.predict(text_features)
 
-    # Predict whether the title is clickbait or not
-    prediction = knn.predict(text_features)
+    return prediction[0]
 
-    # Display the prediction
+# set up the app interface
+st.title('Clickbait Classification with KNN')
+
+# get user input and make prediction
+text_input = st.text_input('Enter a clickbait title:')
+if text_input != '':
+    prediction = predict_clickbait(text_input)
+
+    # display prediction result
     if prediction == 0:
         st.write('Not clickbait')
     else:
