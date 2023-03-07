42b4c6eea9f0d91b621cd4f6d799b32d48b5c7f6
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-03-07 15:51:31 +0700

    change several code

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import re
 import string
 
 # Load the pre-trained model and vectorizer
-model = joblib.load('model/knn_model.pkl')
-vectorizer = joblib.load('model/vectorizer.pkl')
+model = joblib.load('model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
 
 # Function to preprocess text data
 def preprocess_text(text):
@@ -25,13 +25,13 @@ def preprocess_text(text):
 def app():
     st.set_page_config(page_title='Clickbait Classification', page_icon=':newspaper:', layout='wide')
     st.title('Clickbait Classification')
-    st.write('Enter a headline to classify whether it is clickbait or not.')
+    st.write('Masukkan judul berita untuk diklasifikasi apakah itu clickbait atau tidak.')
     
     # Create a text input box for the user to enter a headline
-    user_input = st.text_input('Headline:')
+    user_input = st.text_input('Judul berita:')
     
-    # Classify the headline if the user presses the 'Classify' button
-    if st.button('Classify'):
+    # Classify the headline if the user presses the 'Klasifikasi' button
+    if st.button('Klasifikasi'):
         # Preprocess the user input
         preprocessed_input = preprocess_text(user_input)
         # Convert the preprocessed input into a feature vector
@@ -39,10 +39,10 @@ def app():
         # Make a prediction using the pre-trained model
         prediction = model.predict(feature_vector)[0]
         # Display the prediction to the user
-        if prediction != 0:
-            st.write('This headline is not clickbait.')
+        if prediction == 0:
+            st.write('Judul berita ini bukan clickbait.')
         else:
-            st.write('This headline is clickbait.')
+            st.write('Judul berita ini clickbait.')
     
 # Run the Streamlit app
 if __name__ == '__main__':
