92a2ec02d7220f478538aea5a933220aa1907170
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-06-16 19:52:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,14 +82,14 @@ def app():
         # Bar Chart
         st.markdown("### Bar Chart")
         bar_data = df['label'].value_counts()
-        fig_bar = go.Figure(data=go.Bar(x=bar_data.index, y=bar_data))
+        fig_bar = go.Figure(data=go.Bar(x=df['title'].index, y=df['label']))
         fig_bar.update_layout(title='Bar Chart')
         st.plotly_chart(fig_bar)
     
         # Pie Chart
         st.markdown("### Pie Chart")
         pie_data = df['label'].value_counts()
-        fig_pie = go.Figure(data=go.Pie(labels=pie_data.index, values=pie_data))
+        fig_pie = go.Figure(data=go.Pie(labels=df['label'].index, values=pie_data))
         fig_pie.update_layout(title='Pie Chart')
         st.plotly_chart(fig_pie)
         
