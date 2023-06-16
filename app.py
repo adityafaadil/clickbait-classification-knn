66b7ee7ca85040f2d98b1d17b68783af8751622d
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-06-16 18:59:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,14 +94,14 @@ def app():
 
         # Word cloud for clickbait articles
         clickbait_titles = ' '.join(df[df['label'] == 'clickbait']['title'])
-        clickbait_wordcloud = WordCloud().generate(clickbait_titles)
-        fig = px.imshow(np.array(clickbait_wordcloud.to_image()), title="Word Cloud - Clickbait")
+        clickbait_wordcloud = WordCloud().generate_from_text(clickbait_titles)
+        fig = px.imshow(clickbait_wordcloud.to_array(), title="Word Cloud - Clickbait")
         st.plotly_chart(fig)
 
         # Word cloud for non-clickbait articles
         non_clickbait_titles = ' '.join(df[df['label'] == 'non-clickbait']['title'])
-        non_clickbait_wordcloud = WordCloud().generate(non_clickbait_titles)
-        fig = px.imshow(np.array(non_clickbait_wordcloud.to_image()), title="Word Cloud - Non-clickbait")
+        non_clickbait_wordcloud = WordCloud().generate_from_text(non_clickbait_titles)
+        fig = px.imshow(non_clickbait_wordcloud.to_array(), title="Word Cloud - Non-clickbait")
         st.plotly_chart(fig)
 
 # Run the Streamlit app
