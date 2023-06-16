666ddced5cebb0a20c41fff41560590298098c03
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-06-16 17:28:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,13 +59,9 @@ def app():
     elif page == 'Dashboard':
         st.title('Tampilan Dashboard')
         data = pd.read_csv('dataset/data_bersih.csv')
-        # Display the total number of clickbait and non-clickbait titles
-        clickbait_count = data['label'].sum()
-        non_clickbait_count = len(data) - clickbait_count
-        st.title('Clickbait Dashboard')
-        st.header('Total Titles')
-        st.write(f'Clickbait: {clickbait_count}')
-        st.write(f'Non-Clickbait: {non_clickbait_count}')
+        # Display the loaded data
+        st.header("Data")
+        st.write(data)
 
 # Run the Streamlit app
 if __name__ == '__main__':
