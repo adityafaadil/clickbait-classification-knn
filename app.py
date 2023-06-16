f8e50da1b8a1c9bd5a8c7d347534d14a2a8ac0d4
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-06-16 20:57:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,10 +75,10 @@ def app():
         st.dataframe(df)
         
        # Menggabungkan semua teks berita clickbait
-        clickbait_texts = " ".join(df[df["label"] == "clickbait"]["text"])
+        clickbait_texts = " ".join(df[df["label"] == "clickbait"]["title"])
 
         # Menggabungkan semua teks berita non-clickbait
-        non_clickbait_texts = " ".join(df[df["label"] == "non-clickbait"]["text"])
+        non_clickbait_texts = " ".join(df[df["label"] == "non-clickbait"]["title"])
 
         # Menghitung frekuensi kemunculan kata-kata clickbait
         clickbait_words_freq = Counter(clickbait_texts.split())
