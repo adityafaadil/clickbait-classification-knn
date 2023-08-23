6f53ca781253ca3d9d0f20161784d9e6f5dc5eea
Fadila Aditya <59457046+adityafaadil@users.noreply.github.com>
2023-08-23 14:08:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from collections import Counter
-
-import matplotlib.pyplot as plt
-import plotly.express as px
-from wordcloud import WordCloud
-
 import joblib
 import re
 import string
